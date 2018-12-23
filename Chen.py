f2d146e6532ea8d0a8c3f6555f33bd4f45ce6e79
Ritesh Ray <rayritesh97@gmail.com>
2018-12-23 12:11:11 +0530

    Trial program for the Chen's model.
    A dataset of around 5000 rows is split into 4000 rows and 1000 rows for training and predicting respectively. RMSE is taken for the observations vs target and the mean RMSE value obtained is around 2.77.

---
+++ Chen.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 21 10:02:51 2018
+
+@author: amarnath
+"""
+import numpy as np
+'''for ploting the graphs'''
+import matplotlib.pyplot as plt
+'''pyFTS is a python library to provide methods
+to deal with fuzzy time series prediction'''
+from pyFTS.data import TAIEX  
+
+'''importing grid to create partitions among set'''
+from pyFTS.partitioners import Grid 
+
+'''importing chen for convetionanl fuzzy time series model'''
+
+from pyFTS.models import chen 
+'''getting the whole TAIEX dataframe'''
+data = TAIEX.get_dataframe()  
+
+'''Data Visualistion'''
+plt.plot(data['Date'],data['avg']) 
+
+'''getting target variable'''  
+
+temp=TAIEX.get_data()
+train = temp[1:4000]         
+test = temp[4000:5000]
+'''Universe of Discourse Partitioner'''
+
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=[10,5])
+
+partitioner =Grid.GridPartitioner(data=train,n=10)
+partitioner.plot(ax)
+plt.show()
+'''creating the chen's model'''
+ 
+
+
+ 
+model =chen.ConventionalFTS(name="a",partitioner=partitioner)
+'''fitting data for training''' 
+model.fit(train)       
+''' Time series forecasting''' 
+forecasts = model.predict(test)   
+
+'''visualising the result for having rough idea of accuracy'''
+plt.plot(data['Date'].dt.year[4000:5000],test)            
+plt.plot(data['Date'].dt.year[4000:5000],forecasts)
+def rmse(predictions, targets):
+    return np.sqrt(((predictions - targets) ** 2).mean())
+rmse_value=rmse(forecasts,test)
+print(rmse_value)
+
+print((rmse_value/test.mean())*100)
+
+ 
