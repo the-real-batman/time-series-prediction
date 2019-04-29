0ba34f7598e6216f5e678af1bdf81c953f769b7b
Amarnath <35567383+12abc3@users.noreply.github.com>
2019-04-29 23:50:33 +0530

    Update chen2.py

--- chen2.py
+++ chen2.py
@@ -1,5 +1,9 @@
  
- 
+
+import os
+os.chdir(r"/home/amarnath/Desktop/python")
+train=pd.read_csv("train.csv")
+np.savetxt(r'/home/amarnath/Desktop/python/temp.txt', train.values, fmt='%s')
 
 
 '''
@@ -42,150 +46,199 @@ import matplotlib.pyplot as plt
 from pyFTS.data import TAIEX 
 data1=TAIEX.get_dataframe()
 data2=data1.drop(["Openly","Highest","Lowermost","Close","Volume"],axis=1)
-d = {}    
+dl = {}    
 for i in range(16):
-    d[i] = pd.DataFrame()
-    d[i]=data2[data2["Date"].dt.year==1995+i]
+    dl[i] = pd.DataFrame()
+    dl[i]=data2[data2["Date"].dt.year==1995+i]
 for i in range(16):
-    print(d[i])
+    print(dl[i])
+
+'''
+
+tempn=dl[0][dl[0]["Date"].dt.month<=10]
+
+tempn[-2:-1]['avg']
+test=dl[0][dl[0]["Date"].dt.month>10]
+
+
+
+
+'''
+itr=0
+while(itr<10):
+    train=dl[itr][dl[itr]["Date"].dt.month<=10]
+    test=dl[itr][dl[itr]["Date"].dt.month>10]
+    itr+=1
+
+
+
+
+ 
+    previous=0
+    sum=0
+    for value in train['avg']:
+        sum+=value-previous
+        previous=value
+    abl=round((sum/2)/train.shape[0])
+    #print("abl=")
+    #print((abl))
     
-train=d[0][d[0]["Date"].dt.month<=10]
-test=d[0][d[0]["Date"].dt.month>10]
-
-
-'''
-
-1st step (Defining the universe of discourse and intervals for observations.)
-
-'''
-previous=0
-sum=0
-for value in train['avg']:
-    sum+=value-previous
-    previous=value
-abl=round((sum/2)/train.shape[0])
-print("abl=")
-print((abl))
-
-min_value=train['avg'].min()
-max_value=train['avg'].max()
-
-universe_of_discourse=[min_value,max_value]
-
-u={}
-ending=0
-starting=universe_of_discourse[0]
-i=0;
-while(ending<universe_of_discourse[1]):
-    ending=starting+abl
-    mid=(ending+starting)/2
-    u[i]=[starting,mid,ending]
-    starting=mid
-    i+=1
-
-print(u)
-
-'''
-
-2nd step(Defining fuzzy sets for observations)
-
-'''
-
-# Triangular membership function
-
-def triangular(x, a, b, c):
-    return max( min( (x-a)/(b-a), (c-x)/(c-b) ), 0 )
-
-
-
-universe=np.arange(universe_of_discourse[0],universe_of_discourse[1],0.1) 
-plt.figure(figsize=(15,7))
-
-lines = []
-
-for fuzzy_set in u.keys():
-  memberships = [ triangular(x, *u[fuzzy_set]) for x in universe]
-  # plot the chart
-  tmp, = plt.plot(universe, memberships, label=fuzzy_set)
-  lines.append(tmp)
-
-plt.legend(handles=lines)
-plt.xlabel("Height")
-plt.ylabel("Membership value")
-
-
-for fuzzy_set in u.keys():
- ## print(u[fuzzy_set])
-  print(fuzzy_set, triangular(7000, *u[fuzzy_set]))
-
-
-i=0
-set1=[]
-for value in train['avg']:
-    i=0
-    min1=-1.0;
-    for fuzzy_set in u.keys():
-        if(min1<triangular(value, *u[fuzzy_set])):
-            min1=triangular(value, *u[0])
-            j=i
+    min_value=train['avg'].min()
+    max_value=train['avg'].max()
+    
+    universe_of_discourse=[min_value,max_value]
+    
+    u={}
+    ending=0
+    starting=universe_of_discourse[0]
+    i=0;
+    while(ending<universe_of_discourse[1]):
+        ending=starting+abl
+        mid=(ending+starting)/2
+        u[i]=[starting,mid,ending]
+        starting=mid
         i+=1
-    set1.append(j)
-print(set1)
 
-
-
-'''
-
-step 3(Fuzzifying observations & Establishing FLRGs)
-
-'''
-
-print(len(set1))
-
-list1=list(set1)
-x=0
-FLR=[[] for i in range(512)]
-for value in list1:
-    FLR[x].append(value)
+    #print(u)
+    
+    '''
+    
+    2nd step(Defining fuzzy sets for observations)
+    
+    '''
+    
+    # Triangular membership function
+    
+    def triangular(x, a, b, c):
+        return max( min( (x-a)/(b-a), (c-x)/(c-b) ), 0 )
+    
+    
+    
+    '''
+    universe=np.arange(universe_of_discourse[0],universe_of_discourse[1],0.1) 
+    plt.figure(figsize=(15,7))
+    
+    lines = []
+    
+    for fuzzy_set in u.keys():
+      memberships = [ triangular(x, *u[fuzzy_set]) for x in universe]
+      # plot the chart
+      tmp, = plt.plot(universe, memberships, label=fuzzy_set)
+      lines.append(tmp)
+    
+    plt.legend(handles=lines)
+    plt.xlabel("Height")
+    plt.ylabel("Membership value")
+    '''
+    
+    for fuzzy_set in u.keys():
+        c=0
+     ## print(u[fuzzy_set])
+      #print(fuzzy_set, triangular(4555, *u[fuzzy_set]))
+    
+    
+    i=0
+    list1=[]
+    for value in train['avg']:
+        i=0
+        min1=-1.0;
+        for fuzzy_set in u.keys():
+            if(min1<triangular(value, *u[fuzzy_set])):
+                min1=triangular(value, *u[0])
+                j=fuzzy_set
+            i+=1
+        list1.append(j)
+   # print(set1)
+    
+        
+    
+    
+    '''
+    
+    step 3(Fuzzifying observations & Establishing FLRGs)
+    creating dictionary for storing FLRs
+    
+    '''
+    from collections import defaultdict
+    d= defaultdict(list)
+    x=0
+    for value in list1:
+        d[x].append(value)
+        x=value
+        
      
-    x=value
-print(FLR)
-
-
-'''
-
-step 4(Forecasting.)
-
-'''
-set2=[]
-day=1;
-start_value=4830.895
-while(day<61):
-    i=0
-    min1=-1.0;
-    for fuzzy_set in u.keys():
-        if(min1<triangular(start_value, *u[fuzzy_set])):
-            min1=triangular(start_value, *u[fuzzy_set])
-            j=i
-        i+=1
-    print(j)
-    set2.append(u[FLR[j][0]][1])
-    start_value=u[FLR[j][0]][1]
-    day+=1
+    def sum_n(n):
+        sum1=0
+        for i in range(n+1):
+            sum1+=i
+        return sum1
     
-print(train)    
-print(set2)
-print(test)
-
-
-
+    '''
+    
+    step 4(Forecasting.)
+    
+    '''
+    list2=[]
+    list3=[]
+    day=1;
+    start_value=float(train[-2:-1]['avg'])
+    while(day<=len(test)):
+        i=0
+        j=0
+        min1=-1.0;
+        for fuzzy_set in u.keys():
+            if(min1<triangular(start_value, *u[fuzzy_set])):
+               # print(triangular(start_value, *u[fuzzy_set]))
+                min1=triangular(start_value, *u[fuzzy_set])
+                j=fuzzy_set
+            i+=1
+        #print(start_value)
+       # print("j",j)
+        #set2.append(u[FLR[j][0]][1])
+        sum1=0
+        sum2=0
+        while(len(d[j])==0):
+            j+=1
+        count=1
+        for i in range(len(d[j])):
+            sum1+=u[d[j][i]][1]
+            sum2+=(count*u[d[j][i]][1])/sum_n(len(d[j]))
+        var=sum1/len(d[j])
+        var2=sum2
+        list2.append(var)
+        list3.append(var2)
+        start_value=var
+        day+=1
+        
+    #print(train)    
+    #print(set2)
+    #print(test['avg'])
+    
+    test['avg_predicted']=list2
+    test['weigted_predicted']=list3
+    #plt.hist(test['avg_predicted'],test['avg'])
+    '''
+    calculating the error value and percentage error
+    
+    '''
+    
+    error=0
+    for index, row in test.iterrows():
+        error+=abs(((row['avg']-row['avg_predicted'])/row['avg'])*100)
+         
+            
+        
+    print("error is ",error/test.shape[0])
+        
 '''
 
 chen's Model implemented
 
 '''
 
-
+list3=[45,89,65,9,23,88,77,76,4,4,5,6,789,9,86]
+import pandas as pd
+pd.DataFrame(list3)
 
 
     
@@ -205,7 +258,6 @@ chen's Model implemented
 
  
  
-
 
 
 
